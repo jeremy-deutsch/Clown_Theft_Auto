e0f52c857ca2724b5e67b22e143c2e53bef1782d
Jeremy Deutsch <jeremydeutsch@email.arizona.edu>
2016-10-15 14:11:03 -0700

    Started Working on OtherClownAI

---
+++ Assets/Scripts/OtherClownAI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OtherClownAI : MonoBehaviour {
+
+    public float movementVariance;
+
+    private float tendency;
+
+	// Use this for initialization
+	void Start () {
+        tendency = Random.Range(movementVariance * -1, movementVariance);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
